1f5ffcbcfe25100ffed65322b20e8522a0fe40a0
sm <sm@localhost>
2023-06-01 16:26:22 +0900

    web.xml  수정 소

--- i-market/src/main/java/com/main/controller/Join.java
+++ i-market/src/main/java/com/main/controller/Join.java
@@ -9,13 +9,9 @@ import javax.servlet.http.HttpSession;
 
 import com.main.model.WebMemberDAO;
 import com.main.model.WebMemberDTO;
-import com.oreilly.servlet.MultipartRequest;
-import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
-
 
 public class Join extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 회원가입
